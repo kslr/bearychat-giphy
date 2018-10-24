01fe96d92e667ae47df4a03e1d26193fa18951a3
Kslr <kslrwang@gmail.com>
2018-10-25 00:22:35 +0800

    default listen 0.0.0.0:8002

--- app.js
+++ app.js
@@ -45,7 +45,7 @@ fastify.post('/', async (request, reply) => {
 
 const start = async () => {
   try {
-    await fastify.listen(3000);
+    await fastify.listen(8002, '0.0.0.0');
     fastify.log.info(`server listening on ${fastify.server.address().port}`);
   } catch (err) {
     fastify.log.error(err);
